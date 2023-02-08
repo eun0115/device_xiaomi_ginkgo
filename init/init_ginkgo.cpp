13572ca5d6f422be733e0c3280d8152d9b2eef5b
Gian <gianpaoloestacio5@gmail.com>
2023-02-08 00:54:43 +0000

    ginkgo: Configure Chipset Name
    Signed-off-by: Gian <gianpaoloestacio5@gmail.com>

--- init/init_ginkgo.cpp
+++ init/init_ginkgo.cpp
@@ -75,6 +75,9 @@ void vendor_load_properties()
     // set maintainer flag 
     property_override("ro.rice.maintainer", "_.rasend | eun0115");
 
+    // set chipset flag 
+    property_override("ro.rice.chipset", "Snapdragon 665");
+
     // Set dalvik heap configuration
     string heapstartsize, heapgrowthlimit, heapsize, heapminfree,
 			heapmaxfree, heaptargetutilization;
